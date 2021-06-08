f2d926983c4c64be915a8f8a7309b58ba937d3d1
Lar089 <carolinelara35@gmail.com>
2021-06-08 16:52:55 -0300

    Add project files.

---
+++ RecursividadeAtividdade1/Program.cs
@@ -0,0 +1,13 @@
+﻿using RecursividadeAtividdade1.Recursao;
+using System;
+
+namespace RecursividadeAtividdade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new RecursaoDecrescente(10).str_final);
+        }
+    }
+}
---
+++ RecursividadeAtividdade1/Recursao/RecursaoDecrescente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RecursividadeAtividdade1.Recursao
+{
+    class RecursaoDecrescente
+    {
+        public string str_final = "Saída: ";
+
+        public RecursaoDecrescente(int valor)
+        {
+            ValoresOrdemDecrescente(valor);
+        }
+
+        public void ValoresOrdemDecrescente(int valor)
+        {
+            if(valor > 0)
+            {
+                str_final += valor;
+                valor--;
+                if (valor > 0)
+                {
+                    str_final += ", ";
+                    ValoresOrdemDecrescente(valor);
+                }
+            }
+        }
+    }
+}
